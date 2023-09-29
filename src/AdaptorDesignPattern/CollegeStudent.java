c21a6be23975f814b133a5cde341461f3de820fd
arunvemireddy <vemireddyarun0701@gmail.com>
2023-09-28 19:39:41 -0600

    Automated commit from Jenkins

--- src/AdaptorDesignPattern/CollegeStudent.java
+++ src/AdaptorDesignPattern/CollegeStudent.java
@@ -14,6 +14,7 @@ public class CollegeStudent implements Student{
 		this.surname = surname;
 		this.email = email;
 	}
+	
 
 	@Override
 	public String getName() {
--- src/StrategyDesignPattern/CreditCardPayment.java
+++ src/StrategyDesignPattern/CreditCardPayment.java
@@ -5,15 +5,12 @@ public class CreditCardPayment implements PaymentStrategy{
 	private String cardNumber;
 	private String name;
 	
-	
 
 	public CreditCardPayment(String cardNumber, String name) {
 
 		this.cardNumber = cardNumber;
 		this.name = name;
 	}
-
-
 
 	@Override
 	public void payment(int amount) {
